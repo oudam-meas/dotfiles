0e803cabdcf91b7450f33e0ccc85acdd2d18a6aa
outdam <mstdam@gmail.com>
2021-05-24 15:48:30 +0800

    add export_vars template

---
+++ .export_vars.zsh
@@ -0,0 +1,14 @@
+# ** Environment Paths ** #
+# export PATH="/usr/local/opt/mysql-client/bin:$PATH"
+# export PATH="/usr/local/go/bin:$PATH"
+# export PYENV_ROOT="$HOME/.pyenv"
+# export PATH="$PYENV_ROOT/bin:$PATH"
+# export PATH="$HOME/Library/Python/3.7/bin:$PATH"
+# export PATH="$HOME/.npm-packages/bin:$PATH"
+# export PATH="$HOME/.jenv/bin:$PATH"
+# export ANDROID_HOME=~/Library/Android/sdk/
+# export PATH="$ANDROID_HOME/tools:$PATH"
+# export PATH="$ANDROID_HOME/platform-tools:$PATH"
+# export PATH="$ANDROID_HOME/tools/bin:$PATH"
+# export PYTHON_HOST_PROG=`which python2`
+# export PYTHON3_HOST_PROG=`which python3`
